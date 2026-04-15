45c0bbf0a35a4235f69cdcf3937cafeacba1ec68
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2026-04-15 20:37:35 -0300

    first commit

---
+++ src/main/java/com/tubetv/TubetvApplication.java
@@ -0,0 +1,13 @@
+package com.tubetv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubetvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubetvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tubetv/TubetvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubetv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubetvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
